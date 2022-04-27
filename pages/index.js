d0f1a5c781e87a81a2f46a8aa06e9ad0aa7709e9
tgifernee01~ <anferne.thomas@gmail.com>
2022-04-27 14:42:57 -0400

    commit on 'main'

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.map}>Map</div>
+      <div className={styles.start}>Start</div>
     </div>
   )
 }
